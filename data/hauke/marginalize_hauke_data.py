786e24624deeb9ea3d2933043d78853d6fb1cdaa
ThibeauWouters <thibeauwouters@gmail.com>
2025-07-27 20:58:23 +0200

    Adding Hauke EM data to the repo

--- data/hauke/marginalize_hauke_data.py
+++ data/hauke/marginalize_hauke_data.py
@@ -3,11 +3,13 @@ import h5py
 from bilby.gw.conversion import lambda_1_lambda_2_to_lambda_tilde, lambda_1_lambda_2_to_delta_lambda_tilde
 from bilby.gw.conversion import chirp_mass_and_mass_ratio_to_component_masses
 
-GW_event_list = ["GW170817", 
+GW_event_list = ["GW170817",
+                 "GW170817",
                  "GW190425"
                  ]
 
 filename_list = ["./GW170817/GW170817_result.hdf5",
+                 "./GW170817/GW170817+EM_result.hdf5",
                  "./GW190425/GW190425_result.hdf5"
                  ]
 
@@ -21,6 +23,20 @@ for GW_event, filename in zip(GW_event_list, filename_list):
         posterior = f['posterior']
         data = {key: posterior[key][:] for key in keys_to_extract}
         
+        # Report sampling time
+        sampling_time_seconds = f["sampling_time"][()]
+        
+        # Convert to days, hours, minutes, seconds format
+        days = int(sampling_time_seconds // (24 * 3600))
+        remainder = sampling_time_seconds % (24 * 3600)
+        hours = int(remainder // 3600)
+        remainder %= 3600
+        minutes = int(remainder // 60)
+        seconds = remainder % 60
+        
+        event_id = GW_event if "EM" not in filename else f"{GW_event}+EM"
+        print(f"Sampling time for {event_id}: {days} days, {hours} hours, {minutes} minutes, {seconds:.2f} seconds")
+        
         # Do the conversions for lambda_tilde and delta_lambda_tilde
         mass_1, mass_2 = chirp_mass_and_mass_ratio_to_component_masses(data["chirp_mass"], data["mass_ratio"])
         lambda_tilde = lambda_1_lambda_2_to_lambda_tilde(data["lambda_1"], data["lambda_2"], mass_1, mass_2)
@@ -30,6 +46,9 @@ for GW_event, filename in zip(GW_event_list, filename_list):
         data["delta_lambda_tilde"] = delta_lambda_tilde
         
         # Save it
-        save_name = f"./{GW_event}/{GW_event}_result.npz"
+        if "EM" in filename:
+            save_name = f"./{GW_event}/{GW_event}+EM_result.npz"
+        else:
+            save_name = f"./{GW_event}/{GW_event}_result.npz"
         np.savez(save_name, **data)
-        print(f"Data for {GW_event} saved to {save_name}")
+        print(f"Data for {GW_event} saved to {save_name}\n")
