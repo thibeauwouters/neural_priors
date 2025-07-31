c4c49e3709d90a700b903e5a632eaa69fbe4ca46
ThibeauWouters <thibeauwouters@gmail.com>
2025-07-31 09:12:00 +0200

    New runs and some plots for them

--- postprocessing/run_bayes_factors.sh
+++ postprocessing/run_bayes_factors.sh
@@ -8,12 +8,12 @@ BASE_DIR="../GW_runs/"
 
 GW_EVENT="GW170817"
 echo "Calculating Bayes factors for $GW_EVENT..."
-python get_bayes_factors.py --gw-event $GW_EVENT --population-type gaussian --base-dir $BASE_DIR
+python get_bayes_factors.py --gw-event $GW_EVENT --population-type uniform --base-dir $BASE_DIR
 python get_bayes_factors.py --gw-event $GW_EVENT --population-type gaussian --base-dir $BASE_DIR
 python get_bayes_factors.py --gw-event $GW_EVENT --population-type double_gaussian --base-dir $BASE_DIR
 
 GW_EVENT="GW190425"
 echo "Calculating Bayes factors for $GW_EVENT..."
-python get_bayes_factors.py --gw-event $GW_EVENT --population-type gaussian --base-dir $BASE_DIR
+python get_bayes_factors.py --gw-event $GW_EVENT --population-type uniform --base-dir $BASE_DIR
 python get_bayes_factors.py --gw-event $GW_EVENT --population-type gaussian --base-dir $BASE_DIR
 python get_bayes_factors.py --gw-event $GW_EVENT --population-type double_gaussian --base-dir $BASE_DIR
--- postprocessing/run_cornerplots.sh
+++ postprocessing/run_cornerplots.sh
@@ -4,15 +4,22 @@
 # This script demonstrates how to generate corner plots for different scenarios
 
 # Set common parameters
-GW_EVENT="GW190425"
 BASE_DIR="../GW_runs/"
 
+GW_EVENT="GW190425"
 echo "Generating corner plots for $GW_EVENT..."
+python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-all-params --plot-bns --plot-hauke --convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-all-params --plot-bns --plot-hauke --convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-all-params --plot-bns --plot-hauke --convert-lambdas
 
-# Generate corner plots for each population type
-python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-bns --plot-hauke --convert-lambdas
-python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-bns --plot-hauke --no-convert-lambdas
-python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-bns --plot-hauke --no-convert-lambdas
-python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-bns --plot-hauke --convert-lambdas
-python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-bns --plot-hauke --no-convert-lambdas
-python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-bns --plot-hauke --convert-lambdas
+GW_EVENT="GW170817"
+echo "Generating corner plots for $GW_EVENT..."
+python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-all-params --plot-bns --plot-hauke --convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type uniform --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type gaussian --plot-all-params --plot-bns --plot-hauke --convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-all-params --plot-bns --plot-hauke --no-convert-lambdas
+python cornerplots.py --gw-event $GW_EVENT --population-type double_gaussian --plot-all-params --plot-bns --plot-hauke --convert-lambdas
