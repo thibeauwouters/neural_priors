f9f788a0b1fa13d2251bc10ea178e0ee287ffed8
ThibeauWouters <thibeauwouters@gmail.com>
2025-08-08 10:15:28 +0200

    Masked affine flows trained

--- NFprior/submit_all.sh
+++ NFprior/submit_all.sh
@@ -4,7 +4,7 @@
 COMMON_ARGS_NSF="--batch-size 1024 --learning-rate 5e-4 --max-patience 250 --num-epochs 2000 --n-neurons 256 --n-blocks-per-transform 6 --num-bins 16 --N-samples-training 50000"
 
 # Conservative parameters for Masked Autoregressive Flows (to prevent overfitting)
-COMMON_ARGS_MAF="--glasflow-type MaskedPiecewiseRationalQuadraticAutoregressiveFlow --batch-size 256 --learning-rate 5e-4 --max-patience 200 --num-epochs 1500 --n-neurons 128 --n-blocks-per-transform 3 --num-bins 8 --n-transforms 3 --N-samples-training 50000"
+COMMON_ARGS_MAF="--glasflow-type MaskedAffineAutoregressiveFlow --batch-size 1024 --learning-rate 5e-4 --max-patience 200 --num-epochs 1500 --n-neurons 128 --n-blocks-per-transform 3 --n-transforms 3 --N-samples-training 50000"
 
 # Arrays for iteration
 POPULATIONS=("uniform" "gaussian" "double_gaussian")
