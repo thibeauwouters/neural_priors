191ccc9f954d955d4826f3de09d7c6dcba5774ab
ThibeauWouters <thibeauwouters@gmail.com>
2025-11-06 14:18:02 +0100

    Revamped for Python script for Figure 1 plot

--- Figure1/copy_figs.sh
+++
@@ -1,3 +0,0 @@
-cp Figure1.pdf /Users/Woute029/Documents/Code/projects/eos_source_classification/paper/Inkscape/
-cp Figure1.pdf_tex /Users/Woute029/Documents/Code/projects/eos_source_classification/paper/Inkscape/
-cp Figure1.svg /Users/Woute029/Documents/Code/projects/eos_source_classification/paper/Inkscape/
